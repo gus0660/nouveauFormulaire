d576736cb42db132e2cc6039437d03ad9ab9a52a
gus0660 <guillaumeroch3@gmail.com>
2024-02-14 16:01:48 +0100

    merci pierre

--- main.js
+++ main.js
@@ -2,7 +2,12 @@
 const form = document.querySelector("#signup");
 const usernameEl = document.forms.formValidate.username;
 const nameEl = document.forms.formValidate.name;
-
+const firstNameEl = document.forms.formValidate.firstname;
+const emailEl = document.forms.formValidate.email;
+const dobEl = document.forms.formValidate.dob;
+const  passEl = document.forms.formValidate.password;
+const confEl = document.forms.formValidate['confirm-password'];
+const sexEl = document.forms.formValidate.sex;
 // J'ai besoin d'une fonction qui vérifie si la valeur d'un input est vide
 function isRequired(elementValue) {
   if (elementValue == "") {
@@ -83,37 +88,72 @@ const checkName = () => {
   }
   return valid;
 };
-// Je dois mettre en place un ecouteur d'évenement sur le submit de ma forme qui doit empecher la soumission du formulaire au serveur afin de pouvoir emettre les erreurs coté front sans rechargement de la page
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  checkAge();
-  isBetween();
-  isValidEmail();
-});
-// le userName ne doit pas contenir : root, afpa et deus
-// Les noms et prénoms ne pourront pas contenir de chiffres et devront avoir une taille minimale de 3 caractères et un max de 15.
-// Le mot de passe a huit caractères ou plus. Et il doit contenir 1 caractère minuscule, 1 caractère majuscule, 1 chiffre et au moins un caractère spécial dans cet ensemble ( !@#$%^&*).
-
-
 function isValidEmail(email) {
   const regex =
     /^(?!root@afpa\.fr|afpa@afpa\.com|deus@afpa\.org)(?!.*@yopmail\.com$)[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return regex.test(email);
 }
 // pour valider l'age
-function checkAge() {
-  const dateSaisie = document.querySelector("#dob").value; // je crée une constante qui capture l'age saisi dans le input "#dob"
+function verifAge(element) {
+  const dateSaisie = element.value; // je crée une constante qui capture l'age saisi dans le input "#dob"
   let bd = new Date(dateSaisie); //le crée une let qui est la conversion de la date saisie(dateSaisie) en objet que javascript comprend (new Date)
   let twentyOneDb = new Date(); //création d'une let qui correspond à la date actuelle(de l'ordi) = new Date
   twentyOneDb.setFullYear(twentyOneDb.getFullYear() - 21); //produit la date actuelle(twentyOneDb) arrondie à l'année et y applique l'opération:
   // twentyOneDb je prend et j'arrondi à l'année(.getFullYear()) et j'y fait moins 21 ;;resultat une année de ref.
   if (bd <= twentyOneDb) {
     // showSuccess(dateSaisie);
-    alert("Vous avez plus de 21 ans.");
+    return false
   } else {
-    alert("Vous devez avoir au moins 21 ans pour continuer.");
+    return true
   }
 }
+const checkAge = () => {
+  let valid = false
+  if(!isRequired(dobEl.value)){
+    showError(dobEl, "vous devez fournir un age")
+  }else if(!verifAge(dobEl)){
+    showError(dobEl, "vous n'avez pas l'age")
+  }else {
+    showSuccess(dobEl)
+    valid = true
+  }
+  return valid
+}
+const checkFirstname = () => {
+  let valid = false
+  if(!isRequired(firstNameEl.value)){
+    showError(firstNameEl, "ne peut être vide")
+  }else if(!isNameValid(firstNameEl.value)){
+    showError(firstNameEl, "vous devez respecter un format precis")
+  }else{
+    showSuccess(firstNameEl)
+    valid = true
+  }
+  return valid
+}
+// Je dois mettre en place un ecouteur d'évenement sur le submit de ma forme qui doit empecher la soumission du formulaire au serveur afin de pouvoir emettre les erreurs coté front sans rechargement de la page
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let nameOk = checkName(),
+  userNameOk = checkUserName(),
+  ageOk = checkAge(),
+  firstnameOk = checkFirstname();
+
+let formIsValid = nameOk && userNameOk && ageOk && firstnameOk;
+
+if(formIsValid){
+  console.log('tout est bon pour envoi')
+}
+
+
+});
+// le userName ne doit pas contenir : root, afpa et deus
+// Les noms et prénoms ne pourront pas contenir de chiffres et devront avoir une taille minimale de 3 caractères et un max de 15.
+// Le mot de passe a huit caractères ou plus. Et il doit contenir 1 caractère minuscule, 1 caractère majuscule, 1 chiffre et au moins un caractère spécial dans cet ensemble ( !@#$%^&*).
+
+
+
 // je récapitule les fonctions que j'ai déjas en place :
 // fonction qui vérifie si la valeur d'un input est vide :
 // function isRequired()
@@ -131,45 +171,45 @@ function checkAge() {
 // function checkAge()
 // il faut mettre un écouteur d'évenement sur chaque input pour checker si c'est bon
 // Sélection des éléments d'entrée
-const userNameInput = document.querySelector("#userName");
-const nomInput = document.querySelector("#name");
-const prenomInput = document.querySelector("#firstname");
-const passwordInput = document.querySelector("#password");
-const emailInput = document.querySelector('#email');
-const ageInput = document.querySelector('#dob');
+// const userNameInput = document.querySelector("#userName");
+// const nomInput = document.querySelector("#name");
+// const prenomInput = document.querySelector("#firstname");
+// const passwordInput = document.querySelector("#password");
+// const emailInput = document.querySelector('#email');
+// const ageInput = document.querySelector('#dob');
 
-form.addEventListener('input', function(e) {
-  const target = e.target;
-  if (target === userNameInput) {
-    if (!isRequired(userNameInput.value)) {
-      alert('Le nom d\'utilisateur est requis.');
-    } else if (!isBetween(nom.value.length, 3, 15)) {
-      alert('Le nom d\'utilisateur doit avoir une longueur minimale de 3 caractères et maximale de 15 caractères.');
-  } else if (!isNameValid(userNameInput.value)) {
-      alert('Le nom d\'utilisateur ne peut pas contenir "root", "afpa" ou "deus" et doit uniquement contenir des lettres.');
-    }
-}
-if (target === nomInput) {
-    if (!isRequired(nomInput.value)) {
-      alert('votre Nom est requis.')
-    } else if (!isBetween(nom.value.length, 3, 15)) {
-      alert('Votre Nom doit avoir une longueur minimale de 3 caractères et maximale de 15 caractères.');
-    } else if (!isNameValid(nomInput.value)) {
-      alert('Votre Nom ne peut pas contenir "root", "afpa" ou "deus" et doit uniquement contenir des lettres.')
-    }
-}
-if (target ===prenomInput) {
-  if (prenomInput.length < 3 || prenomInput.length > 15) {
-  alert(
-    "Le prénom doit avoir une longueur minimale de 3 caractères et maximale de 15 caractères."
-  );
-}
-}
-if (target === emailInput) {
-  isValidEmail();
-}
+// form.addEventListener('input', function(e) {
+//   const target = e.target;
+//   if (target === userNameInput) {
+//     if (!isRequired(userNameInput.value)) {
+//       alert('Le nom d\'utilisateur est requis.');
+//     } else if (!isBetween(nom.value.length, 3, 15)) {
+//       alert('Le nom d\'utilisateur doit avoir une longueur minimale de 3 caractères et maximale de 15 caractères.');
+//   } else if (!isNameValid(userNameInput.value)) {
+//       alert('Le nom d\'utilisateur ne peut pas contenir "root", "afpa" ou "deus" et doit uniquement contenir des lettres.');
+//     }
+// }
+// if (target === nomInput) {
+//     if (!isRequired(nomInput.value)) {
+//       alert('votre Nom est requis.')
+//     } else if (!isBetween(nom.value.length, 3, 15)) {
+//       alert('Votre Nom doit avoir une longueur minimale de 3 caractères et maximale de 15 caractères.');
+//     } else if (!isNameValid(nomInput.value)) {
+//       alert('Votre Nom ne peut pas contenir "root", "afpa" ou "deus" et doit uniquement contenir des lettres.')
+//     }
+// }
+// if (target ===prenomInput) {
+//   if (prenomInput.length < 3 || prenomInput.length > 15) {
+//   alert(
+//     "Le prénom doit avoir une longueur minimale de 3 caractères et maximale de 15 caractères."
+//   );
+// }
+// }
+// if (target === emailInput) {
+//   isValidEmail();
+// }
 
-});
+// });
 
 // Ajout d'un écouteur d'événement 'input' à chaque champ
 // userNameInput.addEventListener("input", () => {
